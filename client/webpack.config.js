897b6310b9ad7f7a59bdb71dcb106e5e447ac9eb
Lain <ckisielius@gmail.com>
2024-10-06 11:52:53 -0400

    add css loader and babel

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -49,8 +49,27 @@ module.exports = () => {
     ],
 
     module: {
-      // TODO: add css loader and babel
-      rules: [],
+      // adds css loader and babel
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
+      ],
     },
   };
 };
