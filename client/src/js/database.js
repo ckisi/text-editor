f08e6840323d0ab915eb34cf76506b6755ed1455
Lain <ckisielius@gmail.com>
2024-10-10 11:58:38 -0400

    add GET logic for idb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// accepts some content and adds it to the database
 export const putDb = async (content) => {
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readwrite");
@@ -22,7 +22,15 @@ export const putDb = async (content) => {
   console.log("Data updated", result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error("getDb not implemented");
+// gets all the content from the database
+export const getDb = async () => {
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  console.log("Data updated", result);
+  return result;
+};
 
 initdb();
